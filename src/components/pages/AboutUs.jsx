f483e9f15fc2b46f7561e8d3c6b2fea5f2a3bfbd
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-06-04 16:15:22 +0200

    end

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -2,7 +2,7 @@ export default function aboutUs() {
   return (
     <>
       <div className="container vh-100 d-flex justify-content-center align-items-center">
-        <h1 className="text-center">CHI SIAMO</h1>
+        <h1 className="text-center">CHI SIAMO ?</h1>
       </div>
     </>
   );
--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -2,7 +2,9 @@ export default function HomePage() {
   return (
     <>
       <div className="container vh-100 d-flex justify-content-center align-items-center">
-        <h1 className="text-center">SCOPRI GLI ATTORI PIU' FAMOSI AL MONDO</h1>
+        <h1 className="text-center">
+          HOME DELL'APP PER POST ATTUALMENTE SULLE TORTE
+        </h1>
       </div>
     </>
   );
